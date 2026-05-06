1d5671665a5f3b00c6f46b921497dad0ec3b571a
Michał Anioł <michal.janusz.aniol@gmail.com>
2026-05-06 13:46:39 +0200

    run & user id files isolation

--- dist/index.js
+++ dist/index.js
@@ -931,70 +931,79 @@ var starter;
 })(starter || (starter = {}));
 var starter;
 (function (starter) {
-    const { inner, setStyle, disable, enable } = dom;
-    const memoUserId = (userId) => {
-        core.store.set(storageNames.userId, userId);
-        dom.inner(starter.elements.userId, userId);
-    };
-    const alphabetData = {
-        azSmall: 'qwertyuiopasdfghjklzxcvbnm',
-        azBig: 'QWERTYUIOPASDFGHJKLZXCVBNM',
-        numbers: '1234567890',
-    };
-    const ALPHABET = alphabetData.numbers + alphabetData.azSmall + alphabetData.azBig;
-    const regex = new RegExp(`^[${ALPHABET}]{21}$`);
+    let user;
+    (function (user) {
+        const { inner, setStyle, disable, enable } = dom;
+        const memoUserId = (userId) => {
+            core.store.set(storageNames.userId, userId);
+            dom.inner(starter.elements.userId, userId);
+        };
+        const alphabetData = {
+            azSmall: 'qwertyuiopasdfghjklzxcvbnm',
+            azBig: 'QWERTYUIOPASDFGHJKLZXCVBNM',
+            numbers: '1234567890',
+        };
+        const ALPHABET = alphabetData.numbers + alphabetData.azSmall + alphabetData.azBig;
+        const regex = new RegExp(`^[${ALPHABET}]{21}$`);
+        user.init = async () => {
+            const secure = await queries.secure.getSecure();
+            console.log('%c secure:', 'background:rgb(0, 42, 255); color: #003300', secure);
+            if (secure.command === queries.responseCommand.secure.generateUserId) {
+                const setNewUser = async () => {
+                    const userIdSet = await queries.user.set();
+                    memoUserId(userIdSet.userId);
+                };
+                const getNo = (info, btn) => (text) => {
+                    inner(info, text);
+                    setStyle(info, 'color', 'var(--off_prime_color)');
+                    disable(btn);
+                };
+                const validateUserId = (info, btn) => (event) => {
+                    const value = event.target.value;
+                    const no = getNo(info, btn);
+                    if (value.length < 21) {
+                        no('Za krótki min 21 znaków');
+                        return;
+                    }
+                    if (value.length > 21) {
+                        no('Za długi max 21 znaków');
+                        return;
+                    }
+                    if (!regex.test(value)) {
+                        no('String zawiera niedozwolone znaki');
+                        return;
+                    }
+                    inner(info, 'jest OK.');
+                    setStyle(info, 'color', 'var(--on_second_color)');
+                    enable(btn);
+                };
+                const checkUserId = (info, btn, input, hide) => async () => {
+                    const userIdSet = await queries.user.checkId(input.value);
+                    console.log('%c userIdSet:', 'background: #ffcc00; color: #003300', userIdSet);
+                    const state = userIdSet.command;
+                    const no = getNo(info, btn);
+                    if (state === queries.responseCommand.user.ok) {
+                        memoUserId(input.value);
+                        hide();
+                    }
+                    else {
+                        no('Niema takiego użytkownika');
+                    }
+                };
+                modal.user.show(setNewUser, validateUserId, checkUserId);
+            }
+            else if (secure.command === queries.responseCommand.secure.go) {
+                memoUserId(secure.userId);
+            }
+            if (secure === null) {
+            }
+        };
+    })(user = starter.user || (starter.user = {}));
+})(starter || (starter = {}));
+var starter;
+(function (starter) {
     starter.run = async () => {
-        const secure = await queries.secure.getSecure();
-        console.log('%c secure:', 'background:rgb(0, 42, 255); color: #003300', secure);
-        if (secure.command === queries.responseCommand.secure.generateUserId) {
-            const setNewUser = async () => {
-                const userIdSet = await queries.user.set();
-                memoUserId(userIdSet.userId);
-            };
-            const getNo = (info, btn) => (text) => {
-                inner(info, text);
-                setStyle(info, 'color', 'var(--off_prime_color)');
-                disable(btn);
-            };
-            const validateUserId = (info, btn) => (event) => {
-                const value = event.target.value;
-                const no = getNo(info, btn);
-                if (value.length < 21) {
-                    no('Za krótki min 21 znaków');
-                    return;
-                }
-                if (value.length > 21) {
-                    no('Za długi max 21 znaków');
-                    return;
-                }
-                if (!regex.test(value)) {
-                    no('String zawiera niedozwolone znaki');
-                    return;
-                }
-                inner(info, 'jest OK.');
-                setStyle(info, 'color', 'var(--on_second_color)');
-                enable(btn);
-            };
-            const checkUserId = (info, btn, input, hide) => async () => {
-                const userIdSet = await queries.user.checkId(input.value);
-                console.log('%c userIdSet:', 'background: #ffcc00; color: #003300', userIdSet);
-                const state = userIdSet.command;
-                const no = getNo(info, btn);
-                if (state === queries.responseCommand.user.ok) {
-                    memoUserId(input.value);
-                    hide();
-                }
-                else {
-                    no('Niema takiego użytkownika');
-                }
-            };
-            modal.user.show(setNewUser, validateUserId, checkUserId);
-        }
-        else if (secure.command === queries.responseCommand.secure.go) {
-            memoUserId(secure.userId);
-        }
-        if (secure === null) {
-        }
+        await starter.user.init();
     };
 })(starter || (starter = {}));
 var statistics;
---
+++ src/screens/starter/run/run.ts
@@ -0,0 +1,13 @@
+namespace starter {
+    export const run = async () => {
+        await user.init()
+
+
+
+        // config i porównanie
+
+        // pobieranie danych:
+        // - pytania
+        // - zdjęcia
+    }
+}
