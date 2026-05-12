740a60615f3603143adbbd1024e8c1604fc432bb
Michał Anioł <michal.janusz.aniol@gmail.com>
2026-05-12 09:34:34 +0200

    info in settings

--- src/screens/settings/info/info.ts
+++ src/screens/settings/info/info.ts
@@ -17,12 +17,14 @@ namespace settings {
         }
 
         type StateT = {
-            settingsAppInfoContentHeight: number | null
+            settingsAppInfoContentHeight: number | null,
+            open: boolean,
 
         }
 
         const state: StateT = {
-            settingsAppInfoContentHeight: null
+            settingsAppInfoContentHeight: null,
+            open: false,
         }
 
         export const init = () => {
@@ -31,20 +33,36 @@ namespace settings {
             elements.settingsAppInfoLess = byId('settings-app-info-less') as HTMLElement
             elements.settingsAppInfoContent = byId('settings-app-info-content') as HTMLElement
 
-            const contentBox = elements.settingsAppInfoContent.getBoundingClientRect()
-            state.settingsAppInfoContentHeight = contentBox.height
+            setTimeout(() => {
+                const contentBox = elements.settingsAppInfoContent.getBoundingClientRect()
+                state.settingsAppInfoContentHeight = contentBox.height
+                console.log('%c state.settingsAppInfoContentHeight:', 'background:rgb(3, 169, 61); color: #003300', state.settingsAppInfoContentHeight, state.open)
 
-            setStyle(elements.settingsAppInfoLess, 'display', 'none')
-            setStyle(elements.settingsAppInfoContent, 'height', '0px')
+                setStyle(elements.settingsAppInfoLess, 'display', 'none')
+                setStyle(elements.settingsAppInfoContent, 'height', '0px')
+            }, 100)
         }
+
+        const showInfo = () => {
+            if (state.open) {
+                setStyle(elements.settingsAppInfoLess, 'display', 'none')
+                setStyle(elements.settingsAppInfoMore, 'display', 'initial')
+                setStyle(elements.settingsAppInfoContent, 'height', '0px')
+            } else {
+                setStyle(elements.settingsAppInfoLess, 'display', 'initial')
+                setStyle(elements.settingsAppInfoMore, 'display', 'none')
+                setStyle(elements.settingsAppInfoContent, 'height', `${state.settingsAppInfoContentHeight}px`)
+            }
+            state.open = !state.open
+        }
+
 
         export const active = () => {
-            add(elements.settingsAppInfo, 'click', () => {
-                setStyle(elements.settingsAppInfoContent, 'height', `${state.settingsAppInfoContentHeight}px`)
-            })
-
+            add(elements.settingsAppInfo, 'click', showInfo)
         }
 
-        export const deactivate = () => { }
+        export const deactivate = () => {
+            remove(elements.settingsAppInfo, 'click', showInfo)
+        }
     }
 }
